c8b27438d02decedb803648117693e06962c87c3
may <e.sirurd10@gmail.com>
2017-06-04 17:14:25 +0800

    新增自製服務

---
+++ src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class DataService {
+
+  data: any = [];
+
+  constructor(private http: Http) { }
+
+  loadArticles () {
+    this.http.get('/api/articles.json')
+      .subscribe(res => {
+        this.data = res.json();
+      });
+  }
+}
