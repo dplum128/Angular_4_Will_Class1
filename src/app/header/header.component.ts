8f2a22689abc759e7a058c0d77944041bbd3dfdb
may <e.sirurd10@gmail.com>
2017-06-04 16:03:49 +0800

    08 練習將 [(ngModel)] 雙向繫結改用 [value] 單向繫結的方式完成資料更新，按下 Enter 才可以寫入 keyword 屬性 09 練習 Angular 範本參考變數 10 練習 [class.className] 與 [style.propName] 的用法 11 練習 ngFor 與 ngIf 的結構型指令用法

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,18 +9,34 @@ export class HeaderComponent implements OnInit {
 
   sitename = 'D.Plum App';
   logourl = 'assets/images/logo.png';
+  subTitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  counter = 0;
 
   constructor() {
     setTimeout(() => {
       this.sitename = 'DPlum Web';
     }, 3000);
-   }
+  }
 
   ngOnInit() {
   }
 
-  changeTitle () {
+  changeTitle() {
     this.sitename = 'D.Plum Web';
+  }
+
+  addCounter() {
+    this.counter++;
+  }
+
+  ShowColor(i) {
+    if (i < 5) {
+      return 'lowCount';
+    } else if (i < 10) {
+      return 'midCount';
+    } else {
+      return 'highCount';
+    }
   }
 
 }
